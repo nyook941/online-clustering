941d8dc5a8af6d833aa56f84aed3cbe7cb33b8a8
Kennan Wu <kennan.k.wu@gmail.com>
2024-11-12 21:02:46 -0600

    updated

--- modules/ART2Clusterer.py
+++ modules/ART2Clusterer.py
@@ -4,75 +4,91 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score
 from scipy.optimize import linear_sum_assignment
 import numpy as np
+import ast
 
 class ART2Clusterer:
-    def __init__(self, vigilance, n_features, n_clusters=10, max_samples_per_cluster=4000):
+    def __init__(self, vigilance, n_features, time_series_index, max_clusters = 100):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+        self.predicted_filename = f"predicted_{time_series_index}.csv"
+        self.df = pd.DataFrame(columns=['timestamp', 'predicted', 'ground_truth', 'features'])
+        self.df.to_csv(self.predicted_filename)
+
         self.vigilance = vigilance
-        self.n_clusters = n_clusters
-
-        self.clusters = torch.zeros(n_clusters, max_samples_per_cluster, n_features).to(self.device)
-        self.cluster_data_indicies = torch.full((n_clusters, max_samples_per_cluster), -1).to(self.device)
-        self.cluster_sizes = torch.zeros(n_clusters, dtype=torch.long).to(self.device)
         
-    def _initialize_first_cluster(self, initial_data, i):
-        self.clusters[0, 0, :] = initial_data.to(self.device) # place the mean in the first index
-        self.clusters[0, 1, :] = initial_data.to(self.device) # place the actual data in the cluster
-        
-        self.cluster_sizes[0] = 1
+        self.cluster_means = torch.zeros(max_clusters, n_features).to(self.device)
+        self.cluster_sizes = []
 
-        self.cluster_data_indicies[0, 1] = i
+    def add_to_csv(self, timestamp, predicted, ground_truth_id, features):
+        features_list = features.squeeze().tolist()
+        data = {
+            'timestamp': timestamp,
+            'predicted': predicted,
+            'ground_truth': ground_truth_id,
+            'features': str(features_list)
+        }
+
+        self.df = pd.concat([self.df, pd.DataFrame([data])], ignore_index=True)
+
+    def write_to_csv(self):
+        self.df.to_csv(self.predicted_filename)
+
+    def fit_clusters(self, datapoint):
+        class_id = datapoint['class_id']
+        timestamp = datapoint['timestamp']
+
+        mfccs = ast.literal_eval(datapoint['mfccs'])
+        mfsc = ast.literal_eval(datapoint['mfsc'])
+        features = torch.tensor(mfccs + mfsc).to(self.device)
+
+        if not self.cluster_sizes:
+            self._initialize_first_cluster(class_id, timestamp, features)
+            return
+        distance, cluster_index = self.winner_mean(features)
+        if self.vigilance_test(distance):
+            self.add_to_cluster(timestamp, cluster_index, class_id, features)
+        else:
+            self.create_new_cluster(timestamp, class_id, features)
+        
+    def _initialize_first_cluster(self, ground_truth_id, timestamp, features):
+        self.cluster_sizes.append(1)
+        self.cluster_means[0,:] = features
+
+        self.add_to_csv(timestamp, 0, ground_truth_id, features)
 
     def winner_mean(self, x):
-        valid_means = self.clusters[self.cluster_sizes > 0, 0]
+        valid_means = self.cluster_means[0:len(self.cluster_sizes)]
         euclidean_distances = torch.sqrt(torch.sum(torch.pow(valid_means - x, 2), dim=1))
-        min_index = torch.argmin(euclidean_distances)
-        return valid_means[min_index], euclidean_distances[min_index], min_index
+        min_distance = torch.min(euclidean_distances).item()
+        min_index = torch.argmin(euclidean_distances).item()
+        return min_distance, min_index
     
     def vigilance_test(self, distance_winner):
-        return torch.pow(distance_winner, 2) < self.vigilance
+        return distance_winner ** 2 < self.vigilance
     
-    def create_new_cluster(self, x, i):
+    def create_new_cluster(self, timestamp, class_id, x):
         # get the first unused cluster
-        new_cluster_i = torch.where(self.cluster_sizes == 0)[0] 
-        new_cluster_i = new_cluster_i[0] 
-
-        # assign the index associated with the data point
-        self.cluster_data_indicies[new_cluster_i, 1] = i
+        new_cluster_i = len(self.cluster_sizes)
 
         # assign the mean and actual data to the cluster
-        self.clusters[new_cluster_i, 0] = x.to(self.device)
-        self.clusters[new_cluster_i, 1] = x.to(self.device)
-        self.cluster_sizes[new_cluster_i] = 1
+        self.cluster_sizes.append(1)
+        self.cluster_means[new_cluster_i,:] = x
+        self.add_to_csv(timestamp, new_cluster_i, class_id, x)
 
-    def add_to_cluster(self, x, cluster_i, i):
+    def add_to_cluster(self, timestamp, cluster_index, class_id, x):
         # cacluate the new mean
-        current_size = self.cluster_sizes[cluster_i].item()
-        current_mean = self.clusters[cluster_i, 0]
+        current_size = self.cluster_sizes[cluster_index]
+        current_mean = self.cluster_means[cluster_index]
         new_mean = (current_mean * current_size + x) / (current_size + 1)
-        self.clusters[cluster_i, 0] = new_mean
-
-        # assign the index of the data
-        self.cluster_data_indicies[cluster_i, current_size+1] = i
         
         # assign the data to the cluster
-        self.clusters[cluster_i, current_size + 1, :] = x.to(self.device)
-        self.cluster_sizes[cluster_i] += 1
+        self.cluster_means[cluster_index,:] = new_mean
+        self.cluster_sizes[cluster_index] += 1
+        self.add_to_csv(timestamp, cluster_index, class_id, x)      
 
-    def fit_clusters(self, x, i):
-        x = x.to(self.device)
-        if self.cluster_sizes[0].item() == 0:
-            self._initialize_first_cluster(x, i)
-            return
-        winner, distance, cluster_index = self.winner_mean(x)
-        if self.vigilance_test(distance):
-            self.add_to_cluster(x, cluster_index, i)
-        else:
-            self.create_new_cluster(x, i)
-
-    def calc_accuracy(self, ground_truth_csv):
-        true_labels, cluster_labels, _ = self._get_labels_and_times(ground_truth_csv)
+    def calc_accuracy(self):
+        true_labels = self.df['ground_truth'].tolist()
+        cluster_labels = self.df['predicted'].tolist()
 
         conf_matrix = confusion_matrix(true_labels, cluster_labels)
         
@@ -84,47 +100,30 @@ class ART2Clusterer:
         
         return conf_matrix, accuracy
     
-    def plot_truth_vs_time(self, ground_truth_csv):
-        y_true, y_pred, times = self._get_labels_and_times(ground_truth_csv)
+    def plot_truth_vs_time(self):
+        true_labels = self.df['ground_truth'].values
+        cluster_labels = self.df['predicted'].values
+        times = self.df['timestamp'].values
+
+        times = true_labels * 120 + times
+
+        times = np.sort(times)
+        true_labels = np.sort(true_labels)
+        cluster_labels = np.sort(cluster_labels)
 
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6), sharey=True)
         
-        ax1.scatter(times, y_true, color="blue", alpha=0.6, s=1)
+        ax1.scatter(times, true_labels, color="blue", alpha=0.6, s=1)
         ax1.set_title("Ground Truth Labels")
         ax1.set_xlabel("Time (seconds)")
         ax1.set_ylabel("Class ID")
         
-        ax2.scatter(times, y_pred, color="red", alpha=0.6, s=1)
+        ax2.scatter(times, cluster_labels, color="red", alpha=0.6, s=1)
         ax2.set_title("Predicted Labels")
         ax2.set_xlabel("Time (seconds)")
 
         plt.tight_layout()
         plt.show()
-
-    def _get_labels_and_times(self, ground_truth_csv):
-        df = pd.read_csv(ground_truth_csv)
-        ground_truth_indices = df.iloc[:, 0].values
-        ground_truth_labels = df['class_id'].values
-        timestamps = df['timestamp'].values
-
-        actual_times = ground_truth_labels * 120 + timestamps
-
-        cluster_sizes = self.cluster_sizes.cpu().numpy()
-        cluster_data_indices = [index.cpu().numpy() for index in self.cluster_data_indicies]
-
-        valid_clusters = [i for i, size in enumerate(cluster_sizes) if size > 0]
-
-        cluster_labels = []
-        true_labels = []
-
-        for cluster_i in valid_clusters:
-            indices = cluster_data_indices[cluster_i][1:cluster_sizes[cluster_i]+1]  # Skip the mean
-            for i in indices:
-                true_class = ground_truth_labels[ground_truth_indices == i].item()
-                true_labels.append(true_class)
-                cluster_labels.append(cluster_i)
-
-        return np.sort(true_labels), np.sort(cluster_labels), np.sort(actual_times)
     
 class SmoothingART2Clusterer(ART2Clusterer):
     def __init__(self, vigilance, n_features, n_clusters=10, max_samples_per_cluster=4000, buffer_size=5):
